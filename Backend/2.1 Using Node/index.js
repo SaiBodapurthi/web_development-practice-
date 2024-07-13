678f71007a33262b91b183c9fc00670f1de63601
sai <saipavanbodapurthi@gmail.com>
2024-07-13 19:51:31 +0530

    backend

---
+++ Backend/2.1 Using Node/index.js
@@ -0,0 +1,3 @@
+var a=35;
+var b=34;
+console.log(a+b);
---
+++ Backend/2.3 NPM/index.js
@@ -0,0 +1,11 @@
+// var generateName=require("sillyname");
+
+// import generateName from "sillyname";
+
+// var myNmae=generateName();
+
+// console.log('my new name is '+myNmae);
+
+import {randomSuperhero} from 'superheroes';
+
+console.log("my name is "+randomSuperhero());
