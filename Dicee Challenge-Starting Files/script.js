7e68d5ce0fa2dcfc0e28cbba3c90743e844631b4
sai <saipavanbodapurthi@gmail.com>
2024-07-31 13:03:05 +0530

    changes to dice game

--- Dicee Challenge-Starting Files/script.js
+++ Dicee Challenge-Starting Files/script.js
@@ -1,19 +1,21 @@
-var randomNumber1=Math.floor(Math.random()*6)+1;
-var randomImage1="./images/dice"+randomNumber1+".png";
-var image1=document.querySelectorAll("img")[0];
-image1.setAttribute("src", randomImage1);
+document.getElementById("myButton").onclick = function() {
+    var randomNumber1=Math.floor(Math.random()*6)+1;
+    var randomImage1="./images/dice"+randomNumber1+".png";
+    var image1=document.querySelectorAll("img")[0];
+    image1.setAttribute("src", randomImage1);
 
-var randomNumber2=Math.floor(Math.random()*6)+1;
-var randomImage2="images/dice"+randomNumber2+".png";
-var image2=document.querySelectorAll("img")[1];
-image2.setAttribute("src", randomImage2);
+    var randomNumber2=Math.floor(Math.random()*6)+1;
+    var randomImage2="images/dice"+randomNumber2+".png";
+    var image2=document.querySelectorAll("img")[1];
+    image2.setAttribute("src", randomImage2);
 
-if(randomNumber1===randomNumber2){
-    document.querySelector("h1").innerHTML="Draw!";
-}
-else if(randomNumber1<randomNumber2){
-    document.querySelector("h1").innerHTML="🚩Player 2 wins";
-}
-else if(randomNumber1>randomNumber2){
-    document.querySelector("h1").innerHTML="🚩Player 1 wins";
-}
+    if(randomNumber1===randomNumber2){
+        document.querySelector("h1").innerHTML="Draw!";
+    }
+    else if(randomNumber1<randomNumber2){
+        document.querySelector("h1").innerHTML="🚩Player 2 wins";
+    }
+    else if(randomNumber1>randomNumber2){
+        document.querySelector("h1").innerHTML="🚩Player 1 wins";
+    }
+};
