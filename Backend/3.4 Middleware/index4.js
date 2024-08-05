aa2d342a3e8146ba05c3658d045fefa7914636b8
sai <saipavanbodapurthi@gmail.com>
2024-08-05 22:25:54 +0530

    custom middleware

--- Backend/3.4 Middleware/index4.js
+++ Backend/3.4 Middleware/index4.js
@@ -1,7 +1,30 @@
 import express from "express";
+import bodyParser from "body-parser";
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+function bandNameGenerator(req,res,next){
+  console.log(req.method);
+  bandName = req.body["street"]+req.body["pet"];
+  next();
+}
 
 const app = express();
 const port = 3000;
+var bandName="";
+
+app.use(express.urlencoded({ extended : true}));
+app.use(bandNameGenerator);
+
+app.get("/", (req,res) => {
+  res.sendFile(__dirname + "/public/index.html");
+});
+
+app.post("/submit", (req,res) => {
+  res.send(`<h1>Your Band Name is </h1> ${bandName}`);
+})
 
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
